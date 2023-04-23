72cc3ffcad2d684814dd401f46ecdb6ac11dc502
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-04-23 06:58:51 +0000

    fix ghq repository function

--- fish/functions/ghq_repo.fish
+++ fish/functions/ghq_repo.fish
@@ -1,11 +1,12 @@
 function ghq_repo
     if type -q ghq
+        set -l ghq_root (ghq root)
         if type -q exa
-            set -l select (ghq list | fzf-tmux -p 80% --height 30% --preview='exa -ahl --icons {}')
-            [ -n "$select" ]; and cd (ghq root)"/$select"
+            set -l select (ghq list | fzf-tmux -p 80% --height 30% --preview="exa -ahl --icons $ghq_root/{}")
+            [ -n "$select" ]; and cd "$ghq_root/$select"
         else
             set -l select (ghq list | fzf-tmux -p 80% --height 30%)
-            [ -n "$select" ]; and cd (ghq root)"/$select"
+            [ -n "$select" ]; and cd "$ghq_root/$select"
         end
         commandline -f repaint
     end
