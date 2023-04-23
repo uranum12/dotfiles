f4b365895c2bd748b1c30fdf485fa1bba6834253
uranum12 <113694643+uranum12@users.noreply.github.com>
2023-04-23 06:22:50 +0000

    update ghq repository function

--- fish/functions/ghq_repo.fish
+++ fish/functions/ghq_repo.fish
@@ -1,11 +1,11 @@
 function ghq_repo
     if type -q ghq
         if type -q exa
-            set -l select (ghq list --full-path | fzf-tmux -p 80% --height 30% --preview='exa -ahl --icons {}')
-            [ -n "$select" ]; and cd "$select"
+            set -l select (ghq list | fzf-tmux -p 80% --height 30% --preview='exa -ahl --icons {}')
+            [ -n "$select" ]; and cd (ghq root)"/$select"
         else
-            set -l select (ghq list --full-path | fzf-tmux -p 80% --height 30%)
-            [ -n "$select" ]; and cd "$select"
+            set -l select (ghq list | fzf-tmux -p 80% --height 30%)
+            [ -n "$select" ]; and cd (ghq root)"/$select"
         end
         commandline -f repaint
     end
