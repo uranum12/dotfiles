2fbc303dc16ab7eb8def70f1cac70de53d2237d4
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-05-13 11:42:28 +0900

    add lua_ls settings

---
+++ nvim/after/lsp/lua_ls.lua
@@ -0,0 +1,22 @@
+return {
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+                pathStrict = true,
+                path = { "?.lua", "?/init.lua" },
+            },
+            workspace = {
+                library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
+                    "${3rd}/luv/library",
+                    "${3rd}/busted/library",
+                    "${3rd}/luassert/library",
+                }),
+                checkThirdParty = "Disable",
+            },
+            format = {
+                enable = false,
+            },
+        },
+    },
+}
