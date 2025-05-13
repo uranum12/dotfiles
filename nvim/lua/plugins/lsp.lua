8ddb9fc8f7e0ff33071c1feab4603052b5dc2cbf
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-05-13 11:21:38 +0900

    add null_ls and virtualline config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,12 +5,26 @@ return {
             depends = {
                 "williamboman/mason.nvim",
                 "williamboman/mason-lspconfig.nvim",
+                "nvimtools/none-ls.nvim",
             },
         })
 
         require("mason").setup()
         require("mason-lspconfig").setup()
 
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+            },
+        })
+
         vim.lsp.enable({ "clangd" })
+
+        vim.diagnostic.config({
+            virtual_text = false,
+            virtual_lines = true,
+        })
     end,
 }
