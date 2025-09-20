3221d4a814eb53f7229e03da2381e7545b6591a7
uranum12 <113694643+uranum12@users.noreply.github.com>
2025-09-21 00:59:21 +0900

    fix highlight related to transparent

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -48,6 +48,7 @@ return {
             use_cterm = true,
         })
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 
         require("mini.tabline").setup({
             tabpage_section = "right",
