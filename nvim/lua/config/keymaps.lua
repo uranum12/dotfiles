04ce1789dd90c5e515bbbc7a21a60c8218e44c27
uranum12 <113694643+uranum12@users.noreply.github.com>
2026-05-25 14:59:27 +0900

    fix neovim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -23,13 +23,14 @@ local function set_universal_keymaps(buf)
 
     vim.keymap.set("n", "U", "<c-r>", opts)
 
-    vim.keymap.set("x", "<", "<gv", opts);
+    vim.keymap.set("x", "<", "<gv", opts)
     vim.keymap.set("x", ">", ">gv", opts)
 
     vim.keymap.set("n", "<esc><esc>", "<cmd>HiClear<cr>", opts)
     vim.keymap.set("n", "<leader><leader>", "<cmd>HiWord<cr>", with_desc("highlight the word"))
 
     vim.keymap.set("n", "<leader>q", "<cmd>BufRemove<cr>", with_desc("remove buffer"))
+    vim.keymap.set("n", "<leader>n", "<cmd>NewMD<cr>", with_desc("new markdown file"))
 
     vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', with_desc("yank to clipboard"))
 end
@@ -45,17 +46,19 @@ local function set_normal_keymaps(buf)
     vim.keymap.set("n", "<tab>", "<cmd>bnext<cr>", with_desc("next buffer"))
     vim.keymap.set("n", "<s-tab>", "<cmd>bprev<cr>", with_desc("prev buffer"))
 
-    vim.keymap.set("n", "<leader>e", "<cmd>PickFiles<cr>", with_desc("files (pick)"))
+    vim.keymap.set("n", "<leader>ff", "<cmd>PickFiles<cr>", with_desc("files (pick)"))
+    vim.keymap.set("n", "<leader>fa", "<cmd>PickAllFiles<cr>", with_desc("all files (pick)"))
+    vim.keymap.set("n", "<leader>fo", "<cmd>PickOldFiles<cr>", with_desc("old files (pick)"))
     vim.keymap.set("n", "<leader>g", "<cmd>Pick grep_live<cr>", with_desc("grep (pick)"))
     vim.keymap.set("n", "<leader>b", "<cmd>Pick buffers<cr>", with_desc("buffers (pick)"))
 
-    vim.keymap.set("n", "<leader>w", "<cmd>Files<cr>", with_desc("explorer"))
+    vim.keymap.set("n", "<leader>e", "<cmd>Files<cr>", with_desc("explorer"))
     vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", with_desc("undotree"))
 
     vim.keymap.set("n", "<leader>t", "<cmd>Term<cr>", with_desc("terminal"))
     vim.keymap.set("n", "<leader>m", "<cmd>lua MiniMap.toggle()<cr>", with_desc("minimap"))
 
-    vim.keymap.set("n", "<leader>f", function()
+    vim.keymap.set("n", "<leader>w", function()
         require("conform").format({
             async = true,
             lsp_format = "fallback",
